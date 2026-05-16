07f271aa45c2e28068cfa5d0c986919056bb4da8
IQBALHASAN <iamekbal75@gmail.com>
2026-05-16 23:17:26 +0600

    fix: update Tawk.to script source URL for live chat integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -117,7 +117,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function() {
   var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
   s1.async = true;
-  s1.src = "https://embed.tawk.to/6a0625d51358261c34fe7d6f/1jok07jdf";
+  s1.src = "https://embed.tawk.to/6a08a52acf689a1c314bbc24/1joosacd3";
   s1.charset = "UTF-8";
   s1.setAttribute("crossorigin", "*");
   s0.parentNode.insertBefore(s1, s0);
